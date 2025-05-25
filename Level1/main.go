8771cc4f11c07ab01a5d4c540ddff8e24f30f419
max <golchanskiy23@gmail.com>
2025-05-25 05:35:13 +0300

    5-8 WBTech1 tasks

--- Level1/main.go
+++ Level1/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"math/rand"
 	"os"
 	"os/signal"
 	"sync"
+	"sync/atomic"
 	"syscall"
 	"time"
 )
@@ -42,120 +45,303 @@ func task1() {
 	human.SaySomething("Hello World")
 }
 
-func Square(x int, ch chan int) {
-	ch <- x * x
-}
-
-func task2() []int {
-	arr := []int{2, 4, 6, 8, 10}
-	ch := make(chan int, len(arr))
-	for _, v := range arr {
-		go Square(v, ch)
-	}
-	ans := make([]int, 0)
-	for i := 0; i < len(arr); i++ {
-		ans = append(ans, <-ch)
-	}
-	close(ch)
-	return ans
-}
-
 func workerCreation(ch <-chan int, wg *sync.WaitGroup) {
-	for v := range ch {
-		fmt.Println(v * v)
+	for n := range ch {
+		fmt.Println(n * n)
 		wg.Done()
 	}
+}
+
+func task2() {
+	arr := []int{2, 4, 6, 8, 10}
+	wg := sync.WaitGroup{}
+	ch := make(chan int)
+	// запускаем пул воркеров
+	for i := 0; i < 5; i++ {
+		go workerCreation(ch, &wg)
+	}
+	for i := 0; i < len(arr); i++ {
+		wg.Add(1)
+		ch <- arr[i]
+	}
+	wg.Wait()
 }
 
 func task2_1() {
 	arr := []int{2, 4, 6, 8, 10}
 	wg := sync.WaitGroup{}
-	jobs := make(chan int)
-
-	for i := 1; i <= 5; i++ {
-		go workerCreation(jobs, &wg)
-	}
-
+	mtx := sync.Mutex{}
+	ans := make([]int, 0)
 	for i := 0; i < len(arr); i++ {
-		wg.Add(1)
-		jobs <- arr[i]
-	}
-
-	wg.Wait()
-	close(jobs)
-}
-
-func task3_1(arr []int) int {
-	sum := 0
-	for _, v := range arr {
-		sum += v
-	}
-	return sum
-}
-
-func task3_2(arr []int) int {
-	wg := &sync.WaitGroup{}
-	mtx := &sync.Mutex{}
-	sum := 0
-	for i := 0; i < len(arr); i++ {
-		wg.Add(1)
-		func(num int) {
-			wg.Done()
-
+		go func(num int) {
+			wg.Add(1)
+			square := num * num
 			mtx.Lock()
+			ans = append(ans, square)
 			defer mtx.Unlock()
-			sum += (num * num)
+			defer wg.Done()
 		}(arr[i])
 	}
 	wg.Wait()
-	return sum
+	for i := 0; i < len(ans); i++ {
+		fmt.Println(ans[i])
+	}
 }
 
-func worker(id int, jobs <-chan int, wg *sync.WaitGroup) {
+func task3() {
+	arr := []int{2, 4, 6, 8, 10}
+	sum := 0
+	ch := make(chan int)
+	for _, num := range arr {
+		go func(i int) {
+			ch <- i * i
+		}(num)
+	}
+
+	for range arr {
+		sum += <-ch
+	}
+	fmt.Println(sum)
+}
+
+func task3_1() {
+	arr := []int{2, 4, 6, 8, 10}
+	wg := &sync.WaitGroup{}
+	mtx := &sync.Mutex{}
+	sum := 0
+	for _, num := range arr {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			square := i * i
+			mtx.Lock()
+			defer mtx.Unlock()
+			sum += square
+		}(num)
+	}
+	wg.Wait()
+	fmt.Println(sum)
+}
+
+func workerPool(id int, ch <-chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	for j := range jobs {
-		fmt.Println("worker", id, "processing job", j)
+	for num := range ch {
+		fmt.Printf("Worker with id %d received %d\n", id, num)
 		time.Sleep(1 * time.Second)
 	}
 }
 
 func task4() {
-	stopChan := make(chan os.Signal, 1)
-	signal.Notify(stopChan, syscall.SIGINT, syscall.SIGTERM)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	var numWorkers int
-	fmt.Print("Введите количество воркеров: ")
-	fmt.Scanln(&numWorkers)
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
 
-	jobs := make(chan int, 100)
+	ch := make(chan int, 100)
 	var wg sync.WaitGroup
 
-	for w := 1; w <= numWorkers; w++ {
+	var n int
+	fmt.Print("Print number of pool workers: ")
+	fmt.Scanln(&n)
+
+	for i := 0; i < n; i++ {
 		wg.Add(1)
-		go worker(w, jobs, &wg)
+		go workerPool(i+1, ch, &wg)
 	}
 
 	go func() {
 		counter := 1
 		for {
 			select {
-			case <-stopChan:
-				fmt.Println("\nПолучен сигнал завершения. Остановка отправки работ.")
-				close(jobs)
+			case <-ctx.Done():
+				fmt.Println("Stopping job generator")
+				close(ch)
 				return
 			default:
-				jobs <- counter
+				ch <- counter
 				counter++
-				time.Sleep(500 * time.Millisecond) // Имитация задержки при получении данных
+				time.Sleep(time.Duration(rand.Intn(500)+500) * time.Millisecond)
 			}
 		}
 	}()
 
-	<-stopChan
+	sig := <-sigChan
+	fmt.Println("\nReceived signal:", sig)
+	cancel()
 
 	wg.Wait()
-	fmt.Println("Программа завершила работу.")
+	fmt.Println("All workers finished.")
+}
+
+func worker5(id int, inputCh <-chan int, outputCh chan<- string) {
+	for num := range inputCh {
+		time.Sleep(500 * time.Millisecond)
+		outputCh <- fmt.Sprintf("Channel with id %d and value %d\n", id, num)
+	}
+}
+
+func task5() {
+	const (
+		poolSize = 5
+	)
+	timer := time.NewTimer(10 * time.Second)
+	inputCh := make(chan int)
+	outputCh := make(chan string)
+	wg := sync.WaitGroup{}
+	for i := 0; i < poolSize; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			worker5(i, inputCh, outputCh)
+		}(i)
+	}
+	go func() {
+		counter := 0
+		for {
+			select {
+			case <-timer.C:
+				fmt.Println("Timeout")
+				close(inputCh)
+				return
+			case inputCh <- counter:
+				counter++
+				time.Sleep(250 * time.Millisecond)
+			}
+		}
+	}()
+	go func(wg *sync.WaitGroup) {
+		wg.Wait()
+		close(outputCh)
+	}(&wg)
+	for v := range outputCh {
+		fmt.Println(v)
+	}
+	fmt.Print("All workers finished.")
+}
+
+func worker6_1(ctx context.Context, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Stopping job generator")
+			return
+		}
+	}
+}
+
+func task6_1() {
+	// timer with timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go worker6_1(ctx, &wg)
+
+	wg.Wait()
+}
+
+func worker6_2(ch <-chan struct{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case <-ch:
+			fmt.Println("Stopping job generator ")
+			return
+		default:
+			fmt.Println("It still working!!!")
+			time.Sleep(500 * time.Millisecond)
+		}
+	}
+}
+
+func closeChannel(ch chan struct{}) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Stopping background  context")
+			close(ch)
+			return
+		}
+	}
+}
+
+func task6_2() {
+	// done channel
+	doneCh := make(chan struct{})
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go worker6_2(doneCh, &wg)
+	closeChannel(doneCh)
+	wg.Wait()
+	fmt.Println("All workers finished.")
+}
+
+func worker6_3(ch chan int) {
+	for v := range ch {
+		fmt.Printf("Received %d\n", v)
+	}
+}
+
+func task6_3() {
+	// closing channels
+	// create unbuffered channel
+	ch := make(chan int)
+	go worker6_3(ch)
+	for i := 0; i < 5; i++ {
+		ch <- i * i
+		time.Sleep(1 * time.Second)
+	}
+	close(ch)
+	fmt.Println("All workers finished.")
+}
+
+type SafeMap struct {
+	m   map[int]int
+	mtx *sync.Mutex
+}
+
+func (s *SafeMap) Write(k int, counter *atomic.Int32) {
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+	s.m[k] += int(counter.Load())
+	counter.Add(1)
+}
+
+func task7() {
+	m := SafeMap{m: make(map[int]int), mtx: &sync.Mutex{}}
+	wg := sync.WaitGroup{}
+	var counter atomic.Int32
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			m.Write(i, &counter)
+		}(i)
+	}
+	wg.Wait()
+	for k, v := range m.m {
+		fmt.Println(k, v)
+	}
+}
+
+func setBit(bit, val byte, n int64) int64 {
+	if val == 0 {
+		n &= ^(1 << bit)
+	} else {
+		n |= (1 << bit)
+	}
+	return n
+}
+
+func task8() {
+	var n int64 = 29
+	fmt.Println(setBit(2, 0, n))
 }
 
 func main() {
+	task8()
 }
